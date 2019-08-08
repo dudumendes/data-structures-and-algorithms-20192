c8f13af55c115aa6e2d29c2da9b7cb619ab2e7a0
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-08-08 17:28:31 -0300

    initial configuration

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import {add, multiply} from './math';
+const num1 = 5, num2 = 10;
+console.log('Add: ', add(num1, num2));
+console.log('Multiply: ', multiply(num1, num2));
---
+++ src/math.js
@@ -0,0 +1,6 @@
+export function add(num1, num2) {
+    return num1 + num2;
+  }
+  export function multiply(num1, num2) {
+    return num1 * num2;
+  }
