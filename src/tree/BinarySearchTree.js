5d358cbdf18207b1293829f41820fa29a21536d5
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-08-22 20:37:06 -0300

    add method improvements and new method: search

--- src/tree/BinarySearchTree.js
+++ src/tree/BinarySearchTree.js
@@ -15,17 +15,55 @@ export default class BinarySearchTree {
 
     addNode(node, key) {
         if (key < node.key) {
-            if (!node.hasLeftChild()) {
-                node.leftChild = new Node(key)
+            if (node.hasLeftChild()) {
+                this.addNode(node.leftChild, key)                
             } else {
-                this.addNode(node.leftChild, key)
+                node.leftChild = new Node(key)
+                node.leftChild.parent = node
             }
         } else {
             if(node.hasRightChild()) {
                 this.addNode(node.rightChild, key)
             } else {
                 node.rightChild = new Node(key)
+                node.rightChild.parent = node
             }
         }
     }
+
+    otherAdd(key) {
+        if (this._root == null) {
+           this._root = new Node(key) 
+        } else {
+            this.otherAddNode(this._root, key)
+        }
+    }
+
+    otherAddNode(node, key) {
+        let child = (key < node.key) ? "leftChild" : "rightChild"
+
+        if (node[child]) {
+            this.otherAddNode(node[child], key)                
+        } else {
+            node[child] = new Node(key, node)
+        }
+    }
+
+    search(key) {
+        return this.searchNode(this._root, key)
+    }
+
+    searchNode(node, key) {
+        if (!node) return null
+
+        if (key < node.key)
+            return this.searchNode(node.leftChild, key)
+        
+        if (key > node.key)
+            return this.searchNode(node.rightChild, key)
+
+        return node
+    }
+
+
 }
