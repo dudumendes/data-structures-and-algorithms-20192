209879a95ab6e7b6cf4e5f28eba893cae8ce636b
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-08-29 20:36:28 -0300

    new methods: printIndentedKey and extra: printMenu

--- src/tree/transversalOperations.js
+++ src/tree/transversalOperations.js
@@ -1,1 +1,29 @@
 export const printKey = node => console.log(node.key)
+
+export const printEvenKey = node => {
+    if (node.key % 2 == 0) {
+        console.log(node.key)
+    }
+}
+
+export const printIndetendKey = node => {
+    let buffer = ""
+
+    for(let i = 0; i < node.depth(); i++) {
+        buffer += "- "
+    }
+
+    buffer += node.key
+    console.log(buffer)
+}
+
+export const printMenu = node => {
+    let buffer = ""
+
+    for(let i = 0; i < node.depth(); i++) {
+        buffer += "- "
+    }
+
+    buffer += node.title
+    console.log(buffer)
+}
