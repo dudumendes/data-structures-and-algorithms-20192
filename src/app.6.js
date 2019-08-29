94ba1cfe2c09cb7050a1b5a264279bcb612dbc72
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-08-29 20:35:24 -0300

    preOrder with indented key

---
+++ src/app.6.js
@@ -0,0 +1,10 @@
+import BST from './tree/BinarySearchTree'
+import { printIndetendKey } from './tree/transversalOperations'
+
+let numbers = [13, 27, 10, 8, 11, 40, 22, 30]
+
+let tree = new BST()
+
+numbers.forEach( key => tree.otherAdd(key))
+
+tree.preOrder(printIndetendKey)
